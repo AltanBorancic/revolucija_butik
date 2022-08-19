0e3b7a210252735925eee7ad1909e818aa27800d
Sefik17 <78441706+Sefik17@users.noreply.github.com>
2022-08-19 22:22:49 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,21 +6,21 @@ Config = {
 }
 
 Config.Pedovi = {
-    {72.36, -1398.97, 28.72,"Clothing Shop",290.83,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-711.76, -149.47, 36.42,"Clothing Shop",193.21,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-165.27, -306.78, 38.73,"Clothing Shop",333.7,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {428.72, -800.19, 28.820,"Clothing Shop",82.63,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-829.71, -1073.59, 10.655,"Clothing Shop",218.0,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-1447.53, -234.87, 48.81,"Clothing Shop",138.04,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-1191.85, -768.181, 16.320,"Clothing Shop",164.63,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {11.94, 6513.92, 31.35,"Clothing Shop",55.01,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {1696.86, 4829.57, 41.45,"Clothing Shop",107.02,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {615.09, 2763.21, 41.09,"Clothing Shop",216.73 ,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {1190.32, 2713.72, 37.65,"Clothing Shop",188.57,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-3171.88, 1044.84, 19.86,"Clothing Shop",358.15,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {123.9204, -224.327, 53.557,"Clothing Shop",354.66,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {-1108.66, 2709.441, 18.507,"Clothing Shop",223.47,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
-    {462.1707, -996.547, 30.689 - 1, "Clothing Shop",97.47,"a_f_y_bevhills_01","a_f_y_bevhills_01"},
+    {72.36, -1398.97, 28.72,"Clothing Shop",290.83,"a_f_y_bevhills_01"},
+    {-711.76, -149.47, 36.42,"Clothing Shop",193.21,"a_f_y_bevhills_01"},
+    {-165.27, -306.78, 38.73,"Clothing Shop",333.7,"a_f_y_bevhills_01"},
+    {428.72, -800.19, 28.820,"Clothing Shop",82.63,"a_f_y_bevhills_01"},
+    {-829.71, -1073.59, 10.655,"Clothing Shop",218.0,"a_f_y_bevhills_01"},
+    {-1447.53, -234.87, 48.81,"Clothing Shop",138.04,"a_f_y_bevhills_01"},
+    {-1191.85, -768.181, 16.320,"Clothing Shop",164.63,"a_f_y_bevhills_01"},
+    {11.94, 6513.92, 31.35,"Clothing Shop",55.01,"a_f_y_bevhills_01"},
+    {1696.86, 4829.57, 41.45,"Clothing Shop",107.02,"a_f_y_bevhills_01"},
+    {615.09, 2763.21, 41.09,"Clothing Shop",216.73 ,"a_f_y_bevhills_01"},
+    {1190.32, 2713.72, 37.65,"Clothing Shop",188.57,"a_f_y_bevhills_01"},
+    {-3171.88, 1044.84, 19.86,"Clothing Shop",358.15,"a_f_y_bevhills_01"},
+    {123.9204, -224.327, 53.557,"Clothing Shop",354.66,"a_f_y_bevhills_01"},
+    {-1108.66, 2709.441, 18.507,"Clothing Shop",223.47,"a_f_y_bevhills_01"},
+    {462.1707, -996.547, 30.689 - 1, "Clothing Shop",97.47,"a_f_y_bevhills_01"},
 }
 
 Config.Shops = {
