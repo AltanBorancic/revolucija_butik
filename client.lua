22fb14d448607e0634de2917ede45d4dafdf889f
Sefik17 <78441706+Sefik17@users.noreply.github.com>
2022-08-19 22:20:54 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -193,6 +193,28 @@ Citizen.CreateThread(function()
 end)
 DaLiJeKomandaSkin = false
 
+
+CreateThread(function()
+    for _,v in pairs(Config.Pedovi) do
+      RequestModel(GetHashKey(v[7]))
+      while not HasModelLoaded(GetHashKey(v[7])) do
+        Wait(1)
+      end
+      RequestAnimDict("mini@strip_club@idles@bouncer@base")
+      while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+        Wait(1)
+      end
+      ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+
+      SetEntityInvincible(ped, true)
+      PlaceObjectOnGroundProperly(ped)
+      SetBlockingOfNonTemporaryEvents(ped, true)
+      TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+    end
+end)
+
 RegisterNetEvent("openClothes_shop_target")
 AddEventHandler("openClothes_shop_target", function(skinkomanda)
 	if skinkomanda then
